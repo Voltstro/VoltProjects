88aa73fc64c37e00ce09273b817eb1663cb70c74
Voltstro <me@voltstro.dev>
2022-06-29 00:57:15 +1000

    Build ClientApp on project build

--- src/VoltProjects.Server/ClientApp/vite.config.ts
+++ src/VoltProjects.Server/ClientApp/vite.config.ts
@@ -1,34 +1,37 @@
-import { defineConfig, UserConfig } from 'vite'
-import { terser } from 'rollup-plugin-terser';
-import { resolve } from 'path'
+import {defineConfig, UserConfig} from 'vite'
+import {terser} from 'rollup-plugin-terser';
+import {resolve} from 'path'
 
 export default defineConfig(({command, mode}) => {
-  let config: UserConfig = {
-    build: {
-      rollupOptions: {
-        input: {
-            main: resolve(__dirname, 'src/main.ts')
-        },
-        output: {
-            dir: resolve(__dirname, '..', 'wwwroot', 'dist'),
-            entryFileNames: () => 'site.js',
-            assetFileNames: () => '[name][extname]',
-            sourcemap: false
-        },
-          plugins: [terser({
-              format: {
-                  comments: false
-              },
-              compress: {
-                  defaults: true,
-                  drop_console: true,
-                  drop_debugger: true
-              },
-              ecma: 2020
-          })]
-      },
+    let config: UserConfig = {
+        build: {
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, 'src/main.ts')
+                },
+                output: {
+                    dir: resolve(__dirname, '..', 'wwwroot', 'dist'),
+                    entryFileNames: () => 'site.js',
+                    assetFileNames: () => '[name][extname]',
+                    sourcemap: false
+                },
+            },
+        }
     }
-  }
 
-  return config;
+    if (mode != 'development') {
+        config.build.rollupOptions.plugins = [terser({
+            format: {
+                comments: false
+            },
+            compress: {
+                defaults: true,
+                drop_console: true,
+                drop_debugger: true
+            },
+            ecma: 2020
+        })]
+    }
+
+    return config;
 })
