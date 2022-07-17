18873c0d1f4fe829fbfb381bba98889b0985a55a
Voltstro <me@voltstro.dev>
2022-07-17 20:30:39 +1000

    Add 'active' css class when the about page is being viewed

---
+++ src/VoltProjects.Server/Core/Helper/MvcHelper.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace VoltProjects.Server.Core.Helper;
+
+//Credit:
+//https://levelup.gitconnected.com/using-asp-net-mvc-to-specify-which-element-in-a-navigation-bar-is-active-9c3dac154f9c
+public static class MvcHelper
+{
+    /// <summary>
+    ///     Can add a CSS class if the defined controller(s) and action(s) are currently being used
+    /// </summary>
+    /// <param name="htmlHelper"></param>
+    /// <param name="controllers"></param>
+    /// <param name="actions"></param>
+    /// <param name="cssClass"></param>
+    /// <returns></returns>
+    public static string ActiveClass(this IHtmlHelper htmlHelper, string? controllers = null, string? actions = null,
+        string cssClass = "active")
+    {
+        string? currentController = htmlHelper.ViewContext.RouteData.Values["controller"] as string;
+        string? currentAction = htmlHelper.ViewContext.RouteData.Values["action"] as string;
+
+        string[] acceptedControllers = (controllers ?? currentController ?? "").Split(',');
+        string[] acceptedActions = (actions ?? currentAction ?? "").Split(',');
+        
+        return acceptedControllers.Contains(currentController) && acceptedActions.Contains(currentAction)
+            ? cssClass
+            : "";
+    }
+}
