300c2bfd659060c4578cf5f3fa5098a96838b737
Voltstro <me@voltstro.dev>
2022-07-27 00:22:24 +1000

    Some XML docs

--- src/VoltProjects.Server/Core/SiteCache/Config/VoltProject.cs
+++ src/VoltProjects.Server/Core/SiteCache/Config/VoltProject.cs
@@ -1,17 +1,32 @@
-using System;
+using System.Diagnostics;
 using static System.String;
 
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Local
+// ReSharper disable UnusedMember.Local
 namespace VoltProjects.Server.Core.SiteCache.Config;
 
+/// <summary>
+///     Details on a project to display on VoltProjects
+/// </summary>
+[DebuggerDisplay("Name = {Name}, Path = {_gitPath}, Remote = {GitIsRemote}")]
 public class VoltProject
 {
-    public string Name { get; set; } = Empty;
+    /// <summary>
+    ///     The name of the project
+    /// </summary>
+    public string Name { get; init; } = Empty;
 
-    private string _gitPath = Empty;
+    private readonly string _gitPath = Empty;
+    
+    /// <summary>
+    ///     The git 'path'
+    ///     <para>Path could be a https:// URL, or a local path to a git repo on this system</para>
+    /// </summary>
     public string GitPath
     {
         get => _gitPath;
-        set
+        init
         {
             _gitPath = value;
             if (value.StartsWith("https://"))
@@ -19,14 +34,39 @@ public class VoltProject
         }
     }
 
-    public bool GitIsRemote { get; private set; }
+    /// <summary>
+    ///     Is this a remote git repo (aka not local)
+    /// </summary>
+    public bool GitIsRemote { get; private init; }
     
-    public string GitBranch { get; set; } = Empty;
-    
-    public bool GitUseLatestTag { get; set; }
+    /// <summary>
+    ///     The branch to use if remote
+    /// </summary>
+    public string GitBranch { get; init; } = Empty;
 
-    public string? IconPath { get; set; }
+    /// <summary>
+    ///     Use the latest tag instead of the latest commit on remote cloned repos?
+    /// </summary>
+    public bool GitUseLatestTag { get; init; }
+
+    /// <summary>
+    ///     Path to the icon to use
+    ///     <para>From the root of the repo</para>
+    /// </summary>
+    public string? IconPath { get; init; }
     
-    public string DocsPath { get; set; } = Empty;
-    public string DocsBuildDist { get; set; } = Empty;
+    /// <summary>
+    ///     Where are docs located in the repo
+    ///     <para>From the root of the repo</para>
+    /// </summary>
+    public string DocsPath { get; init; } = Empty;
+    
+    /// <summary>
+    ///     Where are docs built to
+    ///     <para>From <see cref="DocsPath"/></para>
+    /// </summary>
+    public string DocsBuildDist { get; init; } = Empty;
 }
+// ReSharper restore UnusedMember.Local
+// ReSharper restore UnusedAutoPropertyAccessor.Local
+// ReSharper restore UnusedAutoPropertyAccessor.Global
