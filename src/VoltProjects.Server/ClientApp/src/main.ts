78d6527e99946e500a7cd6614986572e603bb6c9
Voltstro <me@voltstro.dev>
2022-07-30 17:19:33 +1000

    Add social links

--- src/VoltProjects.Server/ClientApp/src/main.ts
+++ src/VoltProjects.Server/ClientApp/src/main.ts
@@ -1,3 +1,8 @@
+//Bootstrap
 import 'bootstrap/js/dist/collapse'
+
+//Force include Bootstrap icon fonts
+import 'bootstrap-icons/font/fonts/bootstrap-icons.woff'
+import 'bootstrap-icons/font/fonts/bootstrap-icons.woff2'
 
 import './main.scss'
