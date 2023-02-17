e1cb3aa9953c33364929f245b5699799e13bb5a0
Voltstro <me@voltstro.dev>
2023-02-18 00:03:05 +1000

    Move

--- src/VoltProjects.Client/src/main/main.ts
+++ src/VoltProjects.Client/src/main/main.ts
@@ -6,10 +6,3 @@ import 'bootstrap-icons/font/fonts/bootstrap-icons.woff'
 import 'bootstrap-icons/font/fonts/bootstrap-icons.woff2'
 
 import './scss/main.scss'
-
-//Other main assets
-import './assets/logo-bootstrap.svg'
-import './assets/logo-dotnet.svg'
-import './assets/logo-libgit2sharp.svg'
-import './assets/logo-serilog.svg'
-import './assets/logo-vite.svg'
