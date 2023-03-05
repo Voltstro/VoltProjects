43446d06c1f294e129e45bb85aa0d5d3f2d2dc2a
Voltstro <me@voltstro.dev>
2023-03-05 21:48:19 +1000

    Toc search

--- src/VoltProjects.Client/src/docfx/scripts/toc.tsx
+++ src/VoltProjects.Client/src/docfx/scripts/toc.tsx
@@ -5,6 +5,7 @@ export function renderToc(): void {
 	}
 
 	createClickFunctionsOnElement(tocElement);
+	registerTocEvents();
 }
 
 function createClickFunctionsOnElement(element: HTMLElement): void {
@@ -22,8 +23,6 @@ function createClickFunctionsOnElement(element: HTMLElement): void {
 				} else {
 					iElement.parentElement.setAttribute('class', 'active');
 				}
-					
-				console.log('Click');
 			});
 		}
 
@@ -33,4 +32,57 @@ function createClickFunctionsOnElement(element: HTMLElement): void {
 		}
 
 	});
+}
+
+function registerTocEvents(): void {
+	const tocFilter = document.getElementById('toc-filter') as HTMLInputElement;
+	if (!tocFilter) {
+		return;
+	}
+
+	tocFilter.addEventListener('input', () => onTocFilterTextChange());
+
+	// Set toc filter from local session storage on page load
+	const filterString = sessionStorage?.filterString;
+	if (filterString) {
+		tocFilter.value = filterString;
+		onTocFilterTextChange();
+	}
+
+	function onTocFilterTextChange(): void {
+		const filter = tocFilter.value?.toLocaleLowerCase() || '';
+		if (sessionStorage) {
+			sessionStorage.filterString = filter;
+		}
+
+		const toc = document.getElementById('toc');
+		const anchors = toc.querySelectorAll('a');
+
+		if (filter == '') {
+			anchors.forEach(a => a.parentElement.classList.remove('filtered', 'hide'));
+			return;
+		}
+
+		const filteredLI = new Set<HTMLElement>();
+		anchors.forEach(a => {
+			const text = a.innerText;
+			if (text && text.toLowerCase().indexOf(filter) >= 0) {
+				let e: HTMLElement = a;
+				while (e && e !== toc) {
+					e = e.parentElement;
+					filteredLI.add(e);
+				}
+			}
+		});
+
+		anchors.forEach(a => {
+			const li = a.parentElement;
+			if (filteredLI.has(li)) {
+				li.classList.remove('hide');
+				li.classList.add('filtered');
+			} else {
+				li.classList.add('hide');
+			}
+		});
+	}
 }
