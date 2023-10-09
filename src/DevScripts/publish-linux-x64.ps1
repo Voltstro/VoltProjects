1230b51328d0a96e945cb2206ecbf0f9030699fa
Voltstro <me@voltstro.dev>
2023-10-10 01:00:32 +1000

    Configure local azurite in docker-compose Remove old publish-linux-x64.ps1 script

--- src/DevScripts/publish-linux-x64.ps1
+++
@@ -1,7 +0,0 @@
-Push-Location "$PSScriptRoot/../VoltProjects.Server"
-dotnet publish -c Release -r linux-x64 --no-self-contained
-Pop-Location
-
-Push-Location "$PSScriptRoot/../VoltProjects.Server/bin/Release/net6.0/linux-x64/publish/"
-tar -czvf ../../linux-x64.tar.gz --numeric-owner **/
-Pop-Location 
