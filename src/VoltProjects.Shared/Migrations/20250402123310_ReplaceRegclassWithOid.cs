8a28e62b2b3e2aa202a606acf96b8a5bf8602e13
Voltstro <me@voltstro.dev>
2025-04-02 22:45:40 +1000

    Replace `regclass` datatype with `oid` (for Postgres upgrade)

---
+++ src/VoltProjects.Shared/Migrations/20250402123310_ReplaceRegclassWithOid.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoltProjects.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplaceRegclassWithOid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<uint>(
+                name: "language_configuration",
+                table: "project_page",
+                type: "oid",
+                nullable: false,
+                defaultValueSql: "'english'",
+                oldClrType: typeof(uint),
+                oldType: "regconfig",
+                oldDefaultValueSql: "'english'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<uint>(
+                name: "language_configuration",
+                table: "project_page",
+                type: "regconfig",
+                nullable: false,
+                defaultValueSql: "'english'",
+                oldClrType: typeof(uint),
+                oldType: "oid",
+                oldDefaultValueSql: "'english'");
+        }
+    }
+}
